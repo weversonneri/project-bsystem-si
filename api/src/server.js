362e3c8820873479513fa494878721b418a392c1
weversonneri <weversonneri@outlook.com>
2021-04-17 15:15:42 -0300

    config heroku post

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const { sequelize } = require('./app/models/index');
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -13,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`🔥 Server started at http://localhost:${PORT}`);
+  console.log('🔥 Server started');
 
   sequelize.authenticate().then(() => {
     console.log('✅ Successfully connected with database.');
