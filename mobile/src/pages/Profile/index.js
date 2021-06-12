1adf245d836475b7e919e66f290a3a4e24fe6cdb
weversonneri <weversonneri@outlook.com>
2021-06-12 17:44:59 -0300

    :construction: some ui ajusts

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -7,6 +7,9 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -20,15 +23,16 @@ import { Input } from '../../components/Input';
 import { styles } from './style';
 import colors from '../../styles/colors';
 import api from '../../services/api';
+import fonts from '../../styles/fonts';
 
 const profileValidationSchema = yup.object().shape({
   name: yup
     .string()
     .required('Full name is required'),
-  // email: yup
-  //   .string()
-  //   .email('Please enter valid email')
-  //   .required('Email is required'),
+  email: yup
+    .string()
+    .email('Please enter valid email')
+    .required('Email is required'),
   // password: yup
   //   .string()
   //   .min(6, ({ min }) => `Password must be at least ${min} characters`)
@@ -63,77 +67,91 @@ export function Profile() {
   }
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.title}>
-        <RectButton onPress={() => navigation.goBack()}>
-          <Icon name="arrow-left" style={styles.titleIcon} />
-        </RectButton>
-        <Text style={styles.titleText}>
-          Meu Perfil
-        </Text>
-        <Text style={{ paddingHorizontal: 10 }} />
-      </View>
-
-      <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-
-        <Image
-          source={
-            user.avatar
-              ? { uri: user.url }
-              : { uri: `https://ui-avatars.com/api/?name=${user.name}` }
-          }
-          style={styles.profileImg}
-        />
-
-        <View style={{ flex: 1 }}>
-          <RectButton style={{ width: 50, height: 25, backgroundColor: colors.green }} title="SignOut" onPress={signOut} />
-        </View>
-
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+        enabled
+      >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={styles.form}>
-            <Formik
-              validationSchema={profileValidationSchema}
-              initialValues={{
-                name: '',
-                // email: '',
-                // phoneNumber: '',
-                // password: '',
-                // confirmPassword: '',
-              }}
-              onSubmit={(values) => handleUpdateProfile(values)}
-            >
-              {({ handleSubmit, isValid }) => (
-                <>
-                  <Field
-                    component={Input}
-                    name="name"
-                    icon="user"
-                    placeholder="Digite seu nome"
-                  />
-                  {/* <Field
-                    component={Input}
-                    name="email"
-                    icon="mail"
-                    placeholder="Digite seu email "
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    underlineColorAndroid="transparent"
-                  /> */}
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.scrollListContainer}
+          >
 
-                  <View style={styles.buttonContainer}>
-                    <Button
-                      onPress={handleSubmit}
-                      title="Confirmar alteração"
-                      disabled={!isValid}
+            <View style={styles.container}>
+              <View style={styles.header}>
+                <RectButton onPress={() => navigation.goBack()}>
+                  <Icon name="arrow-left" style={styles.goBackButton} />
+                </RectButton>
+                <Text style={styles.headerText}>
+                  Meu Perfil
+                </Text>
+                <Text style={{ paddingHorizontal: 10 }} />
+              </View>
+
+              <TouchableOpacity style={styles.profileImgContainer}>
+                <Image
+                  source={
+                    user.avatar
+                      ? { uri: user.url }
+                      : { uri: `https://ui-avatars.com/api/?name=${user.name}` }
+                  }
+                  style={styles.profileImg}
+                />
+              </TouchableOpacity>
+
+              <Text style={styles.formTitle}>
+                Dados
+              </Text>
+
+              <Formik
+                validationSchema={profileValidationSchema}
+                initialValues={{
+                  name: '',
+                  email: '',
+                  // phoneNumber: '',
+                  // password: '',
+                  // confirmPassword: '',
+                }}
+                onSubmit={(values) => handleUpdateProfile(values)}
+              >
+                {({ handleSubmit, isValid }) => (
+                  <>
+                    <Field
+                      component={Input}
+                      name="name"
+                      icon="user"
+                      placeholder="Digite seu nome"
                     />
-                  </View>
-                </>
-              )}
-            </Formik>
-          </View>
-        </TouchableWithoutFeedback>
+                    <Field
+                      component={Input}
+                      name="email"
+                      icon="mail"
+                      placeholder="Digite seu email "
+                      keyboardType="email-address"
+                      autoCapitalize="none"
+                      underlineColorAndroid="transparent"
+                    />
 
-      </ScrollView>
-    </View>
+                    <View style={styles.buttonContainer}>
+                      <Button
+                        onPress={handleSubmit}
+                        title="Confirmar alteração"
+                        disabled={!isValid}
+                      />
+                    </View>
+                  </>
+                )}
+              </Formik>
+            </View>
+            <View>
+              <RectButton style={{ width: 50, height: 25, backgroundColor: colors.green }} title="SignOut" onPress={signOut} />
+            </View>
+
+          </ScrollView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </>
   );
 }
--- mobile/src/pages/Profile/style.js
+++ mobile/src/pages/Profile/style.js
@@ -3,32 +3,50 @@ import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 
 export const styles = StyleSheet.create({
+  scrollListContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
-    width: '100%',
+    paddingHorizontal: 40,
+    alignItems: 'center',
   },
-  title: {
+  header: {
     width: '100%',
-    paddingHorizontal: 35,
     paddingVertical: 15,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  titleIcon: {
+  goBackButton: {
     fontSize: 25,
     color: colors.textTitle,
   },
-  titleText: {
+  headerText: {
     fontFamily: fonts.semiBold,
     fontSize: 18,
     lineHeight: 32,
     color: colors.textTitle,
   },
+  profileImgContainer: {
+    marginTop: 10,
+  },
   profileImg: {
-    width: 100,
-    height: 100,
+    width: 110,
+    height: 110,
     borderRadius: 90,
+    borderWidth: 1,
+    borderColor: colors.purple,
+  },
+  formTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    fontFamily: fonts.regular,
+
+    alignSelf: 'flex-start',
+
+    paddingTop: 15,
+    paddingBottom: 3,
   },
   buttonContainer: {
     marginTop: 40,
